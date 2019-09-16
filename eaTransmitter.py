2a576e83404d0da8f0fca708ae95c33d0ab5748f
LARA lab - UEFS <44122821+lablara@users.noreply.github.com>
2019-09-16 12:10:52 +0100

    Add files via upload

---
+++ eaTransmitter.py
@@ -0,0 +1,33 @@
+# *********************************************************************
+# This class received EA and send it to the requesting EAC
+# It is implemented to communicate through the MQTT protocol
+# Author      : Daniel G. Costa
+# E-mail      : danielgcosta@uefs.br
+# Date        : 2019/09/10
+# *********************************************************************
+
+import paho.mqtt.client as mqtt
+from time import sleep
+
+########################################################
+
+class epuMQTT():
+    def __init__(self, ipBroker, epuId):
+        self.broker = ipBroker
+        self.description = "EPU_CityAlarm_" + str(epuId)
+
+        self.clientmqtt = mqtt.Client("")
+
+        print ("MQTT client created!")
+
+    def publishEA (self, eaJSON):
+
+        print ("Broker address:", self.broker)
+        self.clientmqtt.connect(self.broker)
+
+        self.clientmqtt.publish (self.description, eaJSON)  # Associating a "topic" to a "payload"
+
+        sleep(1)
+        self.clientmqtt.disconnect()
+
+        print("Publishing an Emergency Alarm to the MQTT Broker...")
